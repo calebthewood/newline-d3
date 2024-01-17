a5f0bb3cd04645470dc2cc61ed567f2ae7d157e2
Caleb Wood <calebw787@gmail.com>
2024-01-16 21:09:04 -1000

    fix order of operations

--- 03-histogram/index.js
+++ 03-histogram/index.js
@@ -1,79 +1,89 @@
-// 1. Access Data
-const data = await d3.json("../data/my_weather_data.json");
+async function drawBars() {
 
-/** X Axis: Humidity */
-const xAccessor = d => d.humidity;
-/** Y Axis: Length */
-const yAccessor = d => d.length;
-// 2. Create Dimensions
+  // 1. Access Data
+  const data = await d3.json("../data/my_weather_data.json");
 
-const width = 600;
-let dimensions = {
-  width,
-  height: width * 0.6,
-  margin: {
-    top: 30,
-    right: 10,
-    bottom: 50,
-    left: 50
-  }
-};
+  /** X Axis: Humidity */
+  const xAccessor = d => d.humidity;
+  /** Y Axis: Length */
+  const yAccessor = d => d.length;
 
-dimensions.boundedWidth = dimensions.width
-  - dimensions.margin.left
-  - dimensions.margin.right;
-dimensions.boundedHeight = dimensions.height
-  - dimensions.margin.top
-  - dimensions.margin.bottom;
+  // 2. Create Dimensions
 
-// 3. Draw Canvas
+  const width = 600;
+  // const width = window.innerWidth * 0.9;
+  let dimensions = {
+    width: width,
+    height: width * 0.6,
+    margin: {
+      top: 30,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  };
 
-const wrapper = d3.select("#wrapper")
-  .append("svg")
-  .attr("height", dimensions.height)
-  .attr("width", dimensions.width);
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right;
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom;
 
-const bounds = wrapper.append("g")
-  .style("transform", `translate(
-  ${dimensions.margin.left}px
-  ${dimensions.margin.top}px
-)`);
+  // 3. Draw Canvas
 
-// 4. Create Scales
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
 
-const xScale = d3.scaleLinear()
-  .domain(d3.extent(data, xAccessor))
-  .range([0, dimensions.boundedWidth])
-  .nice();
+  const bounds = wrapper.append("g")
+    .style("transform", `translate(${dimensions.margin.left
+      }px, ${dimensions.margin.top
+      }px)`);
 
-const binsGenerator = d3.bin()
-  .domain(xScale.domain())
-  .value(xAccessor)
-  .thresholds(12);
+  // 4. Create Scales
 
-const bins = binsGenerator(data);
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice();
 
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(bins, yAccessor)])
-  .range([dimensions.boundedHeight, 0])
-  .nice();
+  const binsGenerator = d3.bin()
+    .domain(xScale.domain())
+    .value(xAccessor)
+    .thresholds(12);
 
-console.log(bins[0].x1);
+  const bins = binsGenerator(data);
 
-// 5. Draw Chart
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(bins, yAccessor)])
+    .range([dimensions.boundedHeight, 0])
+    .nice();
 
-const binsGroup = bounds.append("g");
-const binGroups = binsGroup.selectAll("g")
-  .data(bins)
-  .join("g");
+  // 5. Draw Chart
 
-const barPadding = 1;
+  const binsGroup = bounds.append("g");
 
-const barRects = binGroups.append("rect")
-  .attr("x", d => xScale(d.x0 + barPadding / 2))
-  .attr("y", d => yScale(yAccessor(d)))
-  .attr("width", d => d3.max([
-    0, xScale(d.x1) - xScale(d.x0) - barPadding
-  ]))
-  .attr("height", d => dimensions.boundedHeight - yScale(yAccessor(d)))
-  .attr("fill", "black");
+
+  const binGroups = binsGroup.selectAll("g")
+    .data(bins)
+    .join("g");
+
+  const barPadding = 1;
+
+  const barRects = binGroups.append("rect")
+    .attr("x", d => xScale(d.x0) + barPadding / 2)
+    .attr("y", d => yScale(yAccessor(d)))
+    .attr("width", d => d3.max([
+      0,
+      xScale(d.x1) - xScale(d.x0) - barPadding
+    ]))
+    .attr("height", d => dimensions.boundedHeight
+      - yScale(yAccessor(d))
+    )
+    .attr("fill", "cornflowerblue");
+
+}
+
+drawBars();
